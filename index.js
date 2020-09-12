efd82fb0a8ffaf765ff28c23b02dca2aa321e005
jjs951213 <jjs951213@naver.com>
2020-09-12 23:01:07 +0900

    16.2.8 render 함수 만들기

--- index.js
+++ index.js
@@ -43,3 +43,20 @@ function reducer(state = initialState, action) {
   }
 
   const store = createStore(reducer);
+
+  
+const render = () => {
+    const state = store.getState(); // 현재 상태를 불러옵니다.
+    // 토글 처리
+    if (state.toggle) {
+      divToggle.classList.add('active');
+    } else {
+      divToggle.classList.remove('active');
+    }
+    // 카운터 처리
+    counter.innerText = state.counter;
+  };
+  
+  
+  
+  render();
