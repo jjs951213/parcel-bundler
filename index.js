64567e36159d1e2e0c5fb1a9549e80e63c4ce2c1
jjs951213 <jjs951213@naver.com>
2020-09-12 23:03:32 +0900

    16.2.10 액션 발생시키기

--- index.js
+++ index.js
@@ -57,6 +57,15 @@ const render = () => {
     counter.innerText = state.counter;
   };
   
+divToggle.onclick = () => {
+    store.dispatch(toggleSwitch());
+  };
+  btnIncrease.onclick = () => {
+    store.dispatch(increase(1));
+  };
+  btnDecrease.onclick = () => {
+    store.dispatch(decrease());
+  };
   
   
   render();
